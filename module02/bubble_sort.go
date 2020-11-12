f5352a5b9b06b13a95a5d6d00574171163d20b4f
Giuseppe <giuseppe@techtribe.nl>
2020-11-12 14:37:56 +0100

    bubble bubble bubble

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -6,6 +6,19 @@ import "sort"
 //
 // Big O: O(N^2), where N is the size of the list
 func BubbleSortInt(list []int) {
+	length := len(list)
+	for j := 0; j < length-1; j++ {
+		swapped := 0
+		for i := 0; i < length-1-j; i++ {
+			if list[i] > list[i+1] {
+				list[i], list[i+1] = list[i+1], list[i]
+				swapped = 1
+			}
+		}
+		if swapped == 0{
+			break
+		}
+	}
 }
 
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
@@ -21,4 +34,17 @@ func BubbleSortPerson(people []Person) {
 // BubbleSort uses the standard library's sort.Interface to sort. Try
 // implementing it for practice.
 func BubbleSort(list sort.Interface) {
+	length := list.Len()
+	for j := 0; j < length-1; j++ {
+		swapped := 0
+		for i := 0; i < length-1-j; i++ {
+			if list.Less(i+1, i) {
+				list.Swap(i, i+1)
+				swapped = 1
+			}
+		}
+		if swapped == 0 {
+			break
+		}
+	}
 }
